04f4e0510005986a7f14f5c4031bde12fa7a017b
ka2ut <ka2ut@mathcs04.richmond.edu>
2020-10-15 17:16:05 -0400

    Created tester.cpp with appropriate tests of Shape.{h, cpp}

---
+++ tester.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Shape.h>
+
+int main() {
+
+    Shape sh("test");
+    
+    std::cout << "Testing getArea() Method:" << std::endl;
+    std::cout << "---------" << std::endl;
+    std::cout << sh.getArea() << std::endl;
+    std::cout << "Expected [0.0]" << std::endl;
+
+    std::cout << "Testing print() Method:" << std::endl;
+    std::cout << "---------" << std::endl;
+    std::cout << sh.print() << std::endl;
+    std::cout << "Expected [test]" << std::endl;
+
+}
