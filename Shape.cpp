2afc80179e8107ca46252a62a112f135dbf993f5
Allison Newman <allison.newman@richmond.edu>
2020-10-16 17:29:58 -0400

    Implemented Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,21 @@
+#ifndef __SHAPE_CPP__
+#define __SHAPE_CPP__
+
+#include "Shape.cpp"
+#include <iostream>
+
+Shape::Shape(std::string shapeName) {}
+Shape::~Shape() {}
+
+double Shape::getArea()
+{
+return 1;
+//return name.getArea();
+}
+
+void Shape::print()
+{
+std::cout << name << std::endl;
+}
+
+#endif
